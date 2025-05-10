4624902a1fd8a18ccb7c9db57311a551e6be1537
Kazantsev Artem <me@lokach.ru>
2025-05-10 23:41:27 +0300

    start rewrite config

--- lua/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-
-    -- Live Server
-    { 'barrett-ruth/live-server.nvim' },
-
-    -- Vim be good game
-    { 'ThePrimeagen/vim-be-good' },
-
-}
--- lua/plugins/lazygit.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'kdheepak/lazygit.nvim',
-    config = function()
-        vim.keymap.set('n', '<leader>g', function()
-            vim.cmd("LazyGit")
-        end)
-    end
-}
